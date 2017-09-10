25cde12cbbb922c9036d14160605d82ad90c2ab0
Anuar Talipov <anuartb@gmail.com>
2017-09-10 11:28:04 +0500

    Added library moment.js (is required to parse time)

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-console.log("Hello world");
-alert("HIIIII");
+function main(){
+  var date = new Date();
+  var d = date.getDate();
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  console.log(date);
+  console.log("Day: " + d);
+  console.log("Hour: " + h);
+  console.log("Minutes: " + m);
+  console.log("Seconds: " + s);
+}
+
+main()
