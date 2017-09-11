6633e75a2943f2ca05d8a51042b6f0ff1d3f2b35
AnuarTB <AnuarTB@gmail.com>
2017-09-11 19:14:05 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 function main(){
-  var date = new Date();
-  var d = date.getDate();
-  var h = date.getHours();
-  var m = date.getMinutes();
-  var s = date.getSeconds();
+  var date = moment().tz("Asia/Seoul").format("dddd");
   console.log(date);
-  console.log("Day: " + d);
-  console.log("Hour: " + h);
-  console.log("Minutes: " + m);
-  console.log("Seconds: " + s);
 }
 
 main()
