9957b11bec9c52d1d803cd094997df84f3adf026
Anuar Talipov <anuartb@gmail.com>
2017-09-13 06:15:45 +0500

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 	var obj = $.getJSON('json/data.json&callback=?', function(result){
-		$.each(result, function(i, field){
-			$('div').append(field + ' ');
-		});
+		console.log(result);
 	});
 });
